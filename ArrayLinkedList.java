ec179e2c2c468622476e3ac1289e2ded5b7552c4
Ryan Chan Shum Hang <rchanshu@scs.ryerson.ca>
2019-01-20 18:32:19 -0500

    implemented constructor

--- ArrayLinkedList.java
+++ ArrayLinkedList.java
@@ -1,2 +1,13 @@
-class ArrayLinkedList{
+class ArrayLinkedList<E>{
+  public static final DEFAULT_SIZE = 5;
+  E[] array;
+  public ArrayLinkedList(){
+    this(DEFAULT_SIZE);
+  }
+
+  public ArrayLinkedList(int intitialCapacity){
+    array = new E[intitialCapacity]
+  }
+
+
 }
